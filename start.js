ad065905787bf004666c62849d1b5f1b01c361e2
ekatalyst <56366181+ekatalyst@users.noreply.github.com>
2019-10-16 22:43:12 +0530

    Update start.js

--- start.js
+++ start.js
@@ -34,7 +34,7 @@ require("./handlers/mail")
 
 // Start our app!
 const app = require('./app') 
-app.set('port', process.env.PORT || 80) 
+app.set('port', process.env.PORT || 3000) 
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`) 
 }) 
